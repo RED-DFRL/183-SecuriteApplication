803f7e8b32886fca8164637afe467d22aba8f618
RED-DFRL <ps01nnt@eduvaud.ch>
2025-11-13 08:37:10 +0100

    Feat(exo auth)

---
+++ nodejstoken/app/routes/Auth.mjs
@@ -0,0 +1,37 @@
+import express from "express";
+import { connectToDatabase } from "../utils/dbUtils.mjs";
+
+
+const router = express.Router();
+
+// Middleware pour la connexion à la base de données
+const connectToDatabaseMiddleware = async (req, res, next) => {
+  try {
+    req.dbConnection = await connectToDatabase();
+    next();
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+router.post('/', connectToDatabaseMiddleware, async (req, res) => {
+  const { username, password } = req.body;
+
+  const queryString = `SELECT * FROM t_users WHERE useName = '${username}' AND usePassword = '${password}'`; 
+
+  try {
+    const [rows] = await req.dbConnection.query(queryString);
+    if (rows.length > 0) {
+      res.status(200).json({ message: "Authentication successful" });
+    } else {
+      res.status(401).json({ error: "Invalid username or password" });
+    }
+  } catch (error) {
+    console.error("Error authenticating user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+export default router;
